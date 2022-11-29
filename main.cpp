fec58abc28c6f7b39811eed6f8b6b84033bf44a2
Wouter Kiesewetter <wouter@rometron.nl>
2022-11-29 19:27:32 +0100

    Updated for windows

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <chrono>
+#include <functional>
 
 #include "day1.h"
 
-int main()
+void RunDay(std::function<void()> f, int runs = 1)
 {
     auto start = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Hello World\n";
-
-    day1::Run("input/day1.txt");
+    for(int i = 0; i < runs; ++i)
+    {
+        f();
+    }
 
     auto stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << " usec" << std::endl;
+    std::cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / runs << " usec average over " << runs << " runs\n" << std::endl;
+}
+
+int main()
+{
+    std::cout << "Advent of code 2022\n\n";
+
+    RunDay([](){day1::Run("input/day1.txt");},1);
 }
--- tools/matrix.h
+++ tools/matrix.h
@@ -31,6 +31,7 @@ namespace matrix
         {
             return m_matrix.size();
         }
+
         int GetWidth() const
         {
             return m_matrix[0].size();
@@ -51,6 +52,7 @@ namespace matrix
                 m_matrix.push_back(temp);
             }
         }
+
         void Print() const
         {
             for(int i = m_matrix.size() - 1; i >=0; --i)
@@ -62,14 +64,17 @@ namespace matrix
                 std::cout << std::endl;
             }
         }
+
         char GetValueOfPoint(int x, int y) const
         {
             return m_matrix[y][x];
         }
+
         void SetValueOfPoint(int x, int y, char value)
         {
             m_matrix[y][x] = value;
         }
+
         int GetOccurencesOfField(char t, std::vector<Coordinate>* pList = nullptr) const
         {
             int result = 0;
@@ -94,6 +99,7 @@ namespace matrix
             }
             return result;
         }
+
         std::vector<Coordinate> GetNeigborsOf(int x, int y, bool includeDiagonal = true) const
         {
             std::vector<Coordinate> results;
@@ -136,6 +142,7 @@ namespace matrix
 
             return results;
         }
+
         int GetOccurencesOfFieldInSelection(char t, const std::vector<Coordinate>& rSelection, std::vector<Coordinate>* pList = nullptr) const
         {
             int result = 0;
@@ -159,18 +166,23 @@ namespace matrix
             return result;
         }
 
+
+
         char GetValueOfPoint(Coordinate c) const
         {
             return GetValueOfPoint(c.GetX(), c.GetY());
         }
+
         void SetValueOfPoint(Coordinate c, char value)
         {
             SetValueOfPoint(c.GetX(), c.GetY(), value);
         }
+
         std::vector<Coordinate> GetNeigborsOf(Coordinate c) const
         {
             return GetNeigborsOf(c.GetX(), c.GetY());
         }
+
     private:
         std::vector<std::vector<char>> m_matrix;
     };
